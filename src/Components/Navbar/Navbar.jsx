ea1727952235b9d035b4c34ebd15ceb342b5f92f
alexisuwimana <alexisuwimana@yahoo.fr>
2024-08-05 00:13:02 +0200

    updated of css one

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -5,20 +5,28 @@ import menu_icon from '../../assets/menu-icon.jpg'
 import { Link } from 'react-scroll';
 
 const Navbar = () => {
-
   const [sticky, setSticky] = useState(false);
+  const [mobileMenu, setMobileMenu] = useState(false);
 
-  useEffect(()=>{
-    window.addEventListener('scroll', ()=>{
+  useEffect(() => {
+    const handleScroll = () => {
       window.scrollY > 50 ? setSticky(true) : setSticky(false);
-    })
-  },[]);
+    };
 
-  const [mobileMenu, setMobileMenu] =useState(false);
-  const toggleMenu = ()=>{
-    mobileMenu ? setMobileMenu(false) : setMobileMenu(true);
-  }
+    window.addEventListener('scroll', handleScroll);
 
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
+  const toggleMenu = () => {
+    setMobileMenu(!mobileMenu);
+  };
+
+  const handleMenuClick = () => {
+    setMobileMenu(false);
+  };
   return (
     <>
     <div className="contact-info">
@@ -26,7 +34,7 @@ const Navbar = () => {
     </div>
     <nav className={`container ${sticky? 'dark-nav' : ''}`}>
         <img src={logo} alt="" className='logo' />
-        <ul className={mobileMenu?'' : 'hide-mobile-menu'}>
+        <ul className={mobileMenu? 'show-mobile-menu' : 'hide-mobile-menu'}>
             <li><Link to='hero' smooth={true} offset={0} duration={500}>Home</Link></li>
             <li><Link to='services' smooth={true} offset={-260} duration={500}>Services</Link></li>
             <li><Link to='about' smooth={true} offset={-150} duration={500}>About</Link></li>
